ba15e0df6e90dab5058224553705b435808269c0
gon <gongorgojo@gmail.com>
2024-06-23 19:50:22 -0300

    fix(router): switched to hashrouter to make it work on gh pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import Root from './routes/Root';
 import './index.css';
 import ErrorPage from './routes/ErrorPage';
 import RecommededPage from './routes/Recommended';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <Root />,
