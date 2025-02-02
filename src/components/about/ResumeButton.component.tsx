fbc4d97a8175daf35a98459510c9a8ed3bf714cf
gon <gongorgojo@gmail.com>
2025-02-01 22:33:44 -0300

    feat(experience): updated experience

--- src/components/about/ResumeButton.component.tsx
+++ src/components/about/ResumeButton.component.tsx
@@ -2,11 +2,14 @@ import RESUMEPDF from 'src/assets/resume.pdf';
 
 function ResumeButton() {
   return (
-    <div className="hover:bg-background-hover-color  text-main-text-color border-2 border-main-text-color rounded p-1.5 absolute top-5 right-5 font-mono md:fixed">
-      <a href={RESUMEPDF} target="_blank" rel="noreferrer">
-        <p>Resume</p>
-      </a>
-    </div>
+    <a
+      href={RESUMEPDF}
+      target="_blank"
+      rel="noreferrer"
+      className="hover:bg-background-hover-color  text-main-text-color border-2 border-main-text-color rounded p-1.5 absolute top-5 right-5 font-mono md:fixed"
+    >
+      Resume
+    </a>
   );
 }
 
